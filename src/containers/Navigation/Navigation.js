150ec8338663334f033c62dd9a2f854846444b6d
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-06-30 17:12:47 +0500

    Added boilerplate for Navigation OCntainer

---
+++ src/containers/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+    constructor(props){
+        super(props);
+    }
+    render() {
+        return(
+            <nav></nav>
+        );
+    }
+}
+
+export default Navigation;
