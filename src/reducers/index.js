445cdb23bac058657bff2907eddaa5520dc0ef6d
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-01 23:16:46 +0500

    wired modal reducer to app

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import {routerReducer} from 'react-router-redux';
+import modal from './modal';
 const rootReducer = combineReducers({
-  routing: routerReducer
+  routing: routerReducer,
+  modal
 });
 
 export default rootReducer;
