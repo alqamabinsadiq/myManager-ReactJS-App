bcfb5d39e233f1b0329f2c6ac0401098d7211a85
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-02 00:24:36 +0500

    added onTouchTap method to header buttons

--- src/components/Navigation/PublicNavigation.js
+++ src/components/Navigation/PublicNavigation.js
@@ -1,22 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import FlatButton from 'material-ui/FlatButton';
-
+import { connect } from 'react-redux';
+import { openModal } from '../../actions/modal';
 class PublicNavigation extends Component {
     constructor(props) {
         super(props);
     }
-
     render() {
         return (
             <div>
                 <FlatButton label="Login" className="NaigationButton"
-                    style={{ color: "white", fontWeight: 600 }} />
+                    style={{ color: "white", fontWeight: 600 }}
+                    onTouchTap={() => {
+                        this.props.openModal("login");
+                    }} />
                 <FlatButton label="Register"
-                    style={{ color: "white", fontWeight: 600 }} />
+                    style={{ color: "white", fontWeight: 600 }}
+                    onTouchTap={() => {
+                        this.props.openModal("register");
+                    }} />
 
             </div>
         );
     }
 }
 
-export default PublicNavigation;
+const mapStateToProps = () => {
+    return {
+    };
+};
+
+PublicNavigation.propTypes = {
+    openModal: PropTypes.func
+};
+
+export default connect(mapStateToProps, { openModal })(PublicNavigation);
