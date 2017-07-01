29cac8234f10f243d668d65e6e8403b58b26783e
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-01 22:36:04 +0500

    Created initial Landing Component

---
+++ src/components/Landing/Landing.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+export const Landing = () => {
+    return (
+        <div id="gtco-header" className="gtco-cover" role="banner">
+            <div className="overlay"></div>
+            <div className="gtco-container">
+                <div className="row">
+                    <div className="col-md-12 col-md-offset-0 text-left">
+                        <div className="row row-mt-15em">
+                            <div className="col-md-7 mt-text animate-box" data-animate-effect="fadeInUp">
+                                <span className="intro-text-small">Welcome to MyManager.</span>
+                                <h1>Providing easy way to manage your Life.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
+};
+
+export default Landing;
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import Navigation from '../Navigation/Navigation';
+import Landing from '../../components/Landing/Landing';
 const App = () => {
     return (
         <div>
             <Navigation />
             <div className="mainContentBody">
-                <div>
-                    <div id="gtco-header" className="gtco-cover" role="banner">
-                        <div className="overlay"></div>
-                        <div className="gtco-container">
-                            <div className="row">
-                                <div className="col-md-12 col-md-offset-0 text-left">
-                                    <div className="row row-mt-15em">
-                                        <div className="col-md-7 mt-text animate-box" data-animate-effect="fadeInUp">
-                                            <span className="intro-text-small">Welcome to MyManager.</span>
-                                            <h1>Providing easy way to manage your Life.</h1>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                <Landing />
             </div>
         </div>
     );
