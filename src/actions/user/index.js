04d0014ee8768fbdc3f3b84c2c037ae7f9ef8bb2
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-03 01:21:28 +0500

    created some initial login action

---
+++ src/actions/user/index.js
@@ -0,0 +1,62 @@
+// import { replace } from 'react-router-redux';
+// import { closeModal } from '../modal/index';
+import Notifications from 'react-notification-system-redux';
+import { focus, change } from 'redux-form';
+import firebase from 'firebase';
+
+export const actions = {
+  SET_USER_TOKEN: 'SET_USER_TOKEN'
+};
+
+export const login = ({email, password}, resolve) => {
+  return (dispatch) => {
+      firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(({ data: user }) => {
+      if (user.status === 200) {
+        sessionStorage.setItem('userId', user.id);
+        sessionStorage.setItem('token', user.token);
+        dispatch(setUserToken(user.token));
+        // Forcing reload to refresh navbar
+        window.location = '/';
+        resolve();
+      } else {
+        const notificationOpts = {
+          title: 'Oops! Something went wrong.',
+          message: user.message,
+          position: 'tr',
+          autoDismiss: 5
+        };
+        dispatch(change('LoginForm', 'password', ''));
+        dispatch(focus('LoginForm', 'password'));
+        dispatch(Notifications.error(notificationOpts));
+        resolve();
+      }
+    })
+    .catch(() => {
+      const notificationOpts = {
+        title: 'Oops! Something went wrong.',
+        message: 'internet problem maybe?',
+        position: 'tr',
+        autoDismiss: 5
+      };
+      dispatch(Notifications.error(notificationOpts));
+      resolve();
+    });
+  };
+};
+
+export const loginUser = ({ email, password }) => {
+    return (dispatch) => {
+        dispatch({ type: LOGIN_USER });
+        firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(user => loginUserSuccess(dispatch, user))
+        .catch(() => {
+            firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(user => loginUserSuccess(dispatch, user))
+            .catch((error) => { 
+                console.log(error.message);
+                loginUserFail(dispatch); 
+            });
+        });
+    }; 
+};
