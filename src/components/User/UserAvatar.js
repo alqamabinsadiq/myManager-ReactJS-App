7eaa4e1d56e0de69217ee9f31156dca51f7202ee
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-05 00:22:06 +0500

    Created Avatar initial component .

---
+++ src/components/User/UserAvatar.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+ /*  import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+// Actions
+import { logout, route } from '../../actions/user';
+// Components
+import IconButton from 'material-ui/IconButton';
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+import Avatar from 'material-ui/Avatar';
+import Logout from 'material-ui/svg-icons/action/exit-to-app';
+import Profile from 'material-ui/svg-icons/social/person'; */
+
+class Avatar extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            
+        );
+    }
+}
+export default Avatar;
