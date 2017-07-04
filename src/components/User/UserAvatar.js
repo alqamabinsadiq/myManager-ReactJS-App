055f9fb9bc07dd0d6fd208cf6396bf5f07228a6f
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-05 00:27:28 +0500

    Implemented IconButton and MenuItem in UserAvatar Component.

--- src/components/User/UserAvatar.js
+++ src/components/User/UserAvatar.js
@@ -1,25 +1,59 @@
-import React, { Component, PropTypes } from 'react';
- /*  import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
 // Actions
-import { logout, route } from '../../actions/user';
+// import { logout, route } from '../../actions/user';
 // Components
-import IconButton from 'material-ui/IconButton';
+// import IconButton from 'material-ui/IconButton';
 import IconMenu from 'material-ui/IconMenu';
 import MenuItem from 'material-ui/MenuItem';
-import Avatar from 'material-ui/Avatar';
+// import Avatar from 'material-ui/Avatar';
 import Logout from 'material-ui/svg-icons/action/exit-to-app';
-import Profile from 'material-ui/svg-icons/social/person'; */
+import Profile from 'material-ui/svg-icons/social/person'; 
 
-class Avatar extends Component {
+// custom styles
+const styles = {
+  anchor: {
+    horizontal: 'left',
+    vertical: 'bottom'
+  },
+  target: {
+    horizontal: 'right',
+    vertical: 'top'
+  },
+  noPadding: {
+    padding: 0
+  }
+};
+
+class UserAvatar extends Component {
     constructor(props) {
         super(props);
     }
 
     render() {
         return(
-            
+            <IconMenu
+        // iconButtonElement={this.iconButtonElement()}
+        anchorOrigin={styles.anchor}
+        targetOrigin={styles.target}
+      >
+        <MenuItem
+          onClick={this.openProfile}
+          leftIcon={
+            <Profile className={styles.icon} hoverColor="#3f4f62" color="#ccc" />
+          }
+          primaryText="Profile"
+        />
+        <MenuItem
+          onClick={this.logout}
+          leftIcon={
+            <Logout className={styles.icon} hoverColor="#3f4f62" color="#ccc" />
+          }
+          primaryText="Sign out"
+        />
+      </IconMenu>
         );
     }
 }
-export default Avatar;
+export default UserAvatar;
