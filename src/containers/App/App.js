59dbb6418e8021909024e6d6619da752893a2512
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-04 21:27:09 +0500

    integrated firebase with App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react';
+import firebase from 'firebase';
+// Components
 import Navigation from '../Navigation/Navigation';
 import LandingComponent from '../../components/Landing/Landing';
 import ModalContainer from '../Modal';
 import NotificationContainer from '../Notification/NotificationContainer';
 
+// Main App Component
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentWillMount() {
+        // Initialize Firebase
+        const config = {
+            apiKey: "AIzaSyCv1YcLZxtQLZErsuH0n7bMOfQMZMCFiyw",
+            authDomain: "mymanager-bec86.firebaseapp.com",
+            databaseURL: "https://mymanager-bec86.firebaseio.com",
+            projectId: "mymanager-bec86",
+            storageBucket: "mymanager-bec86.appspot.com",
+            messagingSenderId: "485887851803"
+        };
+        firebase.initializeApp(config);
+    }
     render() {
         return (
             <div>
