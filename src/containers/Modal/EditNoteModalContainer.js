eb671934e0835cb719c51e96896989e2b4081598
Alqama Bin Sadiq <alqamabinsadiq@gmail.com>
2017-07-13 22:34:30 +0500

    implemented some features of Edit Note.

--- src/containers/Modal/EditNoteModalContainer.js
+++ src/containers/Modal/EditNoteModalContainer.js
@@ -8,7 +8,7 @@ import Dialog from 'material-ui/Dialog';
 import { Field, reduxForm } from 'redux-form';
 //Styles
 import { styles, renderTextField } from './imports';
-
+import TextField from 'material-ui/TextField';
 // Actions
 import { closeModal, openModal } from '../../actions/modal/index';
 
@@ -17,9 +17,13 @@ class LoginFormContainer extends Component {
     constructor(props) {
         super(props);
         this.onCloseHandler = this._onCloseHandler.bind(this);
+        this.renderTextField = this._renderTextField.bind(this);
         this.state = {
             isOpened: true
         };
+    }
+    componentWillMount() {
+        console.log(this.props.note);
     }
     _onCloseHandler() {
         this.setState({
@@ -29,6 +33,16 @@ class LoginFormContainer extends Component {
     }
     submitForm(values) {
         console.log(values);
+    }
+    _renderTextField({value}) {
+        return(
+        <TextField
+            style={{ width: '100%' }}
+            type="text"
+            hintText="Title"
+            floatingLabelText="Title"
+            value={value}
+        />);
     }
     render() {
         const { handleSubmit, pristine, submitting } = this.props;
@@ -45,8 +59,8 @@ class LoginFormContainer extends Component {
                 <div style={{ flex: 1 }}>
                     <form onSubmit={handleSubmit(this.submitForm.bind(this))} className="formContainer">
                         <div className="formInputs">
-                            <Field name="title" component={renderTextField} label="Title" type="text" />
-                            <Field name="note" component={renderTextField} label="Note" type="text" />
+                            <Field name="title" component={this.renderTextField} label="Title" type="text" value={this.props.title} />
+                            <Field name="note" component={renderTextField} label="Note" type="text" value={this.props.note} />
                         </div>
                         <div className="formButtonContainer">
                             <RaisedButton
@@ -69,7 +83,11 @@ LoginFormContainer.propTypes = {
     handleSubmit: PropTypes.func,
     pristine: PropTypes.bool,
     submitting: PropTypes.bool,
-    login: PropTypes.func
+    login: PropTypes.func,
+    note: PropTypes.string,
+    id: PropTypes.string,
+    title: PropTypes.string,
+    index: PropTypes.number
 };
 
 // Here is the validation of Edit note form.
@@ -83,6 +101,15 @@ const validate = values => {
     });
     return errors;
 };
+
+const mapStateToProps = (state) => {
+    return {
+        note: state.notes.currentNote.text,
+        id: state.notes.currentNote.id,
+        title: state.notes.currentNote.title,
+        index: state.notes.currentNote.index
+    };
+};
 const mapDispatchToProps = (dispatch) => {
     return bindActionCreators({
         closeModal,
@@ -93,4 +120,4 @@ const mapDispatchToProps = (dispatch) => {
 export default reduxForm({
     form: 'EditNoteForm',
     validate
-})(connect(null, mapDispatchToProps)(LoginFormContainer));
+})(connect(mapStateToProps, mapDispatchToProps)(LoginFormContainer));
